bdd754234f7629185f4a86fea842f048973added
Bao Nam <bibibaonam@gmail.com>
2016-01-08 09:46:06 +0700

    fixed untracked files

--- attachment.php
+++
@@ -1,1 +0,0 @@
-<?php @header("HTTP/1.1 404 Not found", TRUE, 404); ?>
--- image.php
+++
@@ -1,4 +0,0 @@
-<?php @header("HTTP/1.1 404 Not found", TRUE, 404); ?>
-<script type="text/javascript">
-location.replace('http://www.plbali.com/');
-</script>
